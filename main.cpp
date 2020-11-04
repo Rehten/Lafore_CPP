b0a365dfaf356c31e766a25a56a28d27b29c636d
AASysoev <Chaegh2Oof4phok3yaph>
2020-11-04 19:52:32 +0300

    exercice_4_8

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+struct Vector {
+    float x;
+    float y;
+
+    Vector operator+(Vector &vector) const {
+        return { this->x * vector.y + vector.x * this->y, this->y * vector.y };
+    }
+};
+
+
 int main()
 {
     using namespace std;
+    Vector vector1 { 5.0, 7.0 };
+    Vector vector2 { 6.0, 8.0 };
+    Vector vector3 { vector1 + vector2 };
+
+    cout << vector3.x << "/" << vector3.y;
+
     return 0 ;
 }
